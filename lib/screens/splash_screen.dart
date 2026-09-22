267e40f2d6ddbd3a4d97084b04360b96163a9124
Biva <bivaa005@gmail.com>
2026-09-22 18:04:05 +0600

    Updated Splash Screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -9,98 +9,55 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _animationController;
-  late final Animation<double> _logoScaleAnimation;
-  late final Animation<double> _appNameFadeAnimation;
+class _SplashScreenState extends State<SplashScreen> {
 
-  Timer? _navigationTimer;
-
-  static const Color _backgroundColor = Color(0xFFEFE8DB);
+  double opacity = 0;
 
   @override
   void initState() {
     super.initState();
 
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 2200),
-    );
+    Future.delayed(Duration(milliseconds: 100), () {
+      setState(() {
+        opacity = 1;
+      });
+    });
 
-    _logoScaleAnimation = Tween<double>(begin: 0.9, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: const Interval(0.0, 0.45, curve: Curves.easeOut),
-      ),
-    );
-
-    _appNameFadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: const Interval(0.35, 0.85, curve: Curves.easeIn),
-      ),
-    );
-
-    _animationController.forward();
-
-    _navigationTimer = Timer(const Duration(seconds: 3), _navigateToLogin);
-  }
-
-  void _navigateToLogin() {
-    if (!mounted) return;
-
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const LoginScreen(),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _navigationTimer?.cancel();
-    _animationController.dispose();
-    super.dispose();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LoginScreen(),
+        ),
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-
     return Scaffold(
-      backgroundColor: _backgroundColor,
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            physics: const NeverScrollableScrollPhysics(),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ScaleTransition(
-                  scale: _logoScaleAnimation,
-                  child: Image.asset(
-                    'assets/images/Logo/Lendr_logo.png',
-                    width: 388,
-                    height: 388,
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                Transform.translate(
-                  offset: const Offset(0, -115),
-                  child: FadeTransition(
-                    opacity: _appNameFadeAnimation,
-                    child: Image.asset(
-                      'assets/images/Logo/Logo_text.png',
-                      width: screenWidth * 0.95 > 650 ? 650 : screenWidth * 0.95,
-                      height: 238,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+      backgroundColor: Color(0xFFEFE8DB),
+
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: opacity,
+          duration: Duration(seconds: 1),
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/Logo/Lendr_logo.png',
+                width: 330,
+                height: 330,
+              ),
+
+              Image.asset(
+                'assets/images/Logo/Logo_text.png',
+                width: 400,
+                height: 250,
+              ),
+            ],
           ),
         ),
       ),
