3f150b28a510f364023779276adf41ec2d2d4f1a
SAFIR-007 <safir.cse.00724205101010@aust.edu>
2026-09-23 22:21:45 +0600

    qwe

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'constants/app_colors.dart';
-import 'constants/app_strings.dart';
 import 'screens/home_screen.dart';
 import 'screens/notifications_screen.dart';
 import 'screens/terms_conditions_screen.dart';
